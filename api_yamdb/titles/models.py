9832838d17cdb3912170f7cf6915008d9bb5a37c
Петрук Кирилл <petruk-90-90@mail.ru>
2023-03-15 18:40:00 +0300

    add review model, urls, view and serializer. add custom permission. register apps in settings

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    pass
+
+
+class Title(models.Model):
+    pass
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE)
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE)
+    text = models.TextField()
+    score = models.IntegerField()
+    pub_date = models.DateTimeField(
+        'Дата добавления', auto_now_add=True, db_index=True
+    )
+
+    class Meta:
+        verbos_name = 'review'
